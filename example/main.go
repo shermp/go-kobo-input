fa68fbe62ccf5a57fc242d511898abb0c886f108
shermp <14854761+shermp@users.noreply.github.com>
2018-09-28 18:17:12 +1200

    Added license info

--- example/main.go
+++ example/main.go
@@ -1,3 +1,23 @@
+/*
+	go-kobo-input - Basic touch input handling for Kobo ereaders
+    Copyright (C) 2018 Sherman Perry
+
+    This file is part of go-kobo-input.
+
+    go-kobo-input is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    go-kobo-input is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with go-kobo-input.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
